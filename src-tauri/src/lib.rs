cf4c7b123983953c3e35e4cb721f54b2052030fa
Ryan Lynch <lynchryan2@gmail.com>
2025-02-06 16:15:42 +0000

    Add App Menu Options for enabling or disabling 'Always on Top'

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -31,11 +31,74 @@
 //     "/usr/share/applications",
 // ];
 
+use tauri::{
+    menu::{AboutMetadata, MenuBuilder, MenuItemBuilder, SubmenuBuilder},
+    Manager,
+};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_notification::init())
         .invoke_handler(tauri::generate_handler![])
+        .setup(|app| {
+            // my custom settings menu item
+            let enable_always_on_top_menu_item = MenuItemBuilder::new("Enable Always On Top")
+                .id("enable-always-on-top")
+                .accelerator("CmdOrControl+,")
+                .build(app)?;
+
+            let disable_always_on_top_menu_item = MenuItemBuilder::new("Disable Always On Top")
+                .id("disable-always-on-top")
+                .accelerator("CmdOrControl+.")
+                .build(app)?;
+
+            // my custom app submenu
+            let app_submenu = SubmenuBuilder::new(app, "App")
+                .about(Some(AboutMetadata {
+                    ..Default::default()
+                }))
+                .separator()
+                .item(&enable_always_on_top_menu_item)
+                .item(&disable_always_on_top_menu_item)
+                .separator()
+                .services()
+                .separator()
+                .hide()
+                .hide_others()
+                .quit()
+                .build()?;
+
+            // ... any other submenus
+
+            let menu = MenuBuilder::new(app)
+                .items(&[
+                    &app_submenu,
+                    // ... include references to any other submenus
+                ])
+                .build()?;
+
+            // set the menu
+            app.set_menu(menu)?;
+
+            // listen for menu item click events
+            let app_handle = app.handle().clone();
+            app.on_menu_event(move |_app, event| {
+                let app_handle = app_handle.clone();
+                let window = app_handle.get_webview_window("main").unwrap();
+                if event.id() == "enable-always-on-top" {
+                    if let Err(e) = window.set_always_on_top(true) {
+                        eprintln!("Failed to enable always on top: {}", e);
+                    }
+                } else if event.id() == "disable-always-on-top" {
+                    if let Err(e) = window.set_always_on_top(false) {
+                        eprintln!("Failed to disable always on top: {}", e);
+                    }
+                }
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
