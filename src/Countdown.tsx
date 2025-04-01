0aff8bfa5432b859900356f32d360095fdc2b1dc
Ryan Lynch <lynchryan2@gmail.com>
2025-04-01 18:12:14 +0100

    cleanup

--- src/Countdown.tsx
+++ src/Countdown.tsx
@@ -23,51 +23,33 @@ const handleNonPluralInterval = (interval: string, value: number): string => {
     return value === 1 ? interval.slice(0, -1) + " " : interval;
 };
 
+/**
+ * Countdown component that displays a countdown timer to a specified target date.
+ * It calculates the time left and updates every second.
+ * When the countdown reaches zero, it shows a notification and changes the display.
+ * @param targetDate - The target date to count down to.
+ */
 const Countdown: React.FC<CountdownProps> = ({ targetDate }) => {
-    const calculateTimeLeft = (): TimeLeft => {
-        const difference = dayjs(targetDate).diff(dayjs());
-        let timeLeft: TimeLeft;
+    
 
-        if (difference > 0) {
-            timeLeft = {
-                // days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-                minutes: Math.floor((difference / 1000 / 60) % 60),
-                seconds: Math.floor((difference / 1000) % 60),
-                milliseconds: Math.floor(difference % 1000),
-            };
-        } else {
-            timeLeft = {
-                // days: 0,
-                hours: 0,
-                minutes: 0,
-                seconds: 0,
-                milliseconds: 0,
-            };
-        }
-
-        return timeLeft;
-    };
-
-    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(targetDate));
     const [notificationSent, setNotificationSent] = useState(false);
 
     useEffect(() => {
-        const now = dayjs();
-        const millisecondsUntilNextSecond = 1000 - now.millisecond();
+        const updateTimer = () => {
+            setTimeLeft(calculateTimeLeft(targetDate));
 
-        const timeout = setTimeout(() => {
-            setTimeLeft(calculateTimeLeft());
-        }, millisecondsUntilNextSecond);
+            const now = dayjs();
+            const millisecondsUntilNextSecond = now.millisecond() % 1000;
 
-        const interval = setInterval(() => {
-            setTimeLeft(calculateTimeLeft());
-        }, 1000);
+            setTimeout(updateTimer, 1000 - millisecondsUntilNextSecond);
+        };
 
-        // Cleanup interval and timeout on component unmount or effect re-run
+        updateTimer();
+
+        // Cleanup function to cancel any pending timeouts
         return () => {
-            clearTimeout(timeout);
-            clearInterval(interval);
+            // No explicit cleanup needed for recursive setTimeout
         };
     }, []);
 
@@ -111,12 +93,16 @@ const Countdown: React.FC<CountdownProps> = ({ targetDate }) => {
             {timeLeft.hours > 0 ||
             timeLeft.minutes > 0 ||
             timeLeft.seconds > 0 ? (
+                // If the countdown is still running, show the countdown timer
+                // with a green background and a pulsing animation
                 <div className="bg-green-800 text-white p-6 rounded-lg shadow-lg w-[250px] max-w-[250px] h-[132px] flex-col items-center justify-center content-center animate-[pulse_4s_ease-in-out_infinite]">
                     {timerComponents.map((component, index) => (
                         <div key={index}>{component}</div>
                     ))}
                 </div>
             ) : (
+                // If the countdown is over, show a flashing red box
+                // with a message "Time's up!"
                 <div className="bg-red-500 w-[250px] max-w-[250px] h-[132px] p-6 flex-col content-center font-semibold text-white rounded-lg shadow-lg">
                     <span className="text-lg font-semibold animate-[ping_1s_ease-in-out_infinite]">
                         Time's up!
@@ -125,6 +111,31 @@ const Countdown: React.FC<CountdownProps> = ({ targetDate }) => {
             )}
         </div>
     );
+};
+
+const calculateTimeLeft = (targetDate: Dayjs): TimeLeft => {
+    const difference = dayjs(targetDate).diff(dayjs());
+    let timeLeft: TimeLeft;
+
+    if (difference > 0) {
+        timeLeft = {
+            // days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+            hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+            minutes: Math.floor((difference / 1000 / 60) % 60),
+            seconds: Math.floor((difference / 1000) % 60),
+            milliseconds: Math.floor(difference % 1000),
+        };
+    } else {
+        timeLeft = {
+            // days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+            milliseconds: 0,
+        };
+    }
+
+    return timeLeft;
 };
 
 export default Countdown;
